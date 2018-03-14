3aa023e0b11dc65aa6efb6bb36bbfc5aa1c54fa9
OVCoder <citryn85@gmail.com>
2018-03-13 23:59:55 -0700

    updated files

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+var profile = require('./profile');
 
 const app = express();
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/profile', profile);
+app.use(express.static(__dirname + '/public'));
+
 
 //Here we're setting the views directory to be ./views
 //thereby letting the app know where to find the template files
@@ -15,7 +19,7 @@ app.set('views', './views');
 
 //Here we're setting the default engine to be ejs
 //note we don't need to require it, express will do that for us.
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');// 'ejs' tells express which template engine to use
 
 //Now instead of using res.send we can use
 //res.render to send the output of the template by filename
@@ -27,9 +31,17 @@ app.get('/', (req, res) =>{
             lastName: 'Volodko',
         }
     }
-  res.render('index', data);
+  res.render('index', data); //here 'index' is the view argument, which is a string that is the file path of the view file to render.
 });
 
+app.get('/contact',(req, res) => {
+    res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+    res.render('thanks', { contact: req.body })  
+});
+   
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
 });
